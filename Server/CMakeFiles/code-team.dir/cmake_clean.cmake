289d671fb20246214c7e4f231ae7bd208a5423c9
Oleg <olezhkabobrov@gmail.com>
2021-03-01 22:24:56 +0300

    Renamed

---
+++ Server/CMakeFiles/code-team.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code-team.dir/src/main.cpp.obj"
+  "code-team.exe"
+  "code-team.exe.manifest"
+  "code-team.pdb"
+  "libcode-team.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code-team.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Server/CMakeFiles/code-team.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for code-team.
+# This may be replaced when dependencies are built.
