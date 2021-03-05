91385d0a4970d0bf0273a22d4a7b907c104735f7
Oleg <olezhkabobrov@gmail.com>
2021-03-05 20:14:50 +0300

    Changed Tool.h

--- Server/include/Tool.h
+++ Server/include/Tool.h
@@ -2,37 +2,12 @@
 #include "InitialData.h"
 #include <string>
 
-struct Position {
-    int left_upper_corner;
-    int right_lower_corner;
-};
-
 class Tool {
 private:
     std::string tool_text;
-    const int size_on_screen = 1;
-    /* size_on_screen is measured in blocks (1-6):
-            *****************
-            *       *       *
-            *   1   *   2   *
-            *       *       *
-            *****************
-            *       *       *
-            *   3   *   4   *
-            *       *       *
-            *****************
-            *       *       *
-            *   5   *   6   *
-            *       *       *
-            *****************
-     */
-    Position position;
 
 public:
     Tool(std::string text, int size);
-    int get_size();
-    void set_position(Position position_);
-    Position get_position();
     std::string get_text();
     virtual ~Tool() = default;
 };
