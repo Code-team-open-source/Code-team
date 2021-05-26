ac8f0a7abfa734186b36e5246757302808e9e7a3
AlexanderShvedov <5661685a@mail.ru>
2021-05-27 02:06:55 +0300

    fix mistake

--- Client/game_field.cpp
+++ Client/game_field.cpp
@@ -10,41 +10,17 @@ Game_field::Game_field(QWidget *parent) :
     ui(new Ui::Game_field)
 {
 
-//    client = new protocol;
-//    client->SendString("ABOBA");
+    client = new protocol;
+    client->SendString("ABOBA");
     ui->setupUi(this);
     ind = new Main_indicators();
     ui->verticalLayout_2->addWidget(ind->box);
     h1 = new QHBoxLayout;
     h2 = new QHBoxLayout;
     task.resize(11);
-//    for (int i = 0; i < 6 ; ++i ) {
-//        printf("got hera %d\n", i);
-//      task[i] = client->GetTool(i);
-//    }
-
-//    for (int i = 0; i < 3 ; ++i ) {
-//        h1->addWidget(task[i]->gr);
-//        h2->addWidget(task[i + 3]->gr);
-//    }
-//    ui->v4->addLayout(h1);
-    for (int i = 0; i < 10 ; ++i ) {
-        int k = rand() % 5;
-        if (k == 0) {
-            task[i] = new Task_sliders();
-        }
-        if (k == 1) {
-            task[i] = new Task_button();
-        }
-        if (k == 2) {
-            task[i] = new Task_buttons_6();
-        }
-        if (k == 3) {
-            task[i] = new Task_dial();
-        }
-        if (k == 4) {
-            task[i] = new Task_git_tool();
-        }
+    for (int i = 0; i < 6 ; ++i ) {
+        printf("got hera %d\n", i);
+      task[i] = client->GetTool(i);
     }
 
     for (int i = 0; i < 3 ; ++i ) {
@@ -53,7 +29,6 @@ Game_field::Game_field(QWidget *parent) :
     }
     ui->v4->addLayout(h1);
     ui->v4->addLayout(h2);
-//    ui->v4->addLayout(h2);
 
     m_player = new QMediaPlayer();
     m_playlist = new QMediaPlaylist(m_player);
