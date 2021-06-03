e2914967e434d5e1825bc0c69aea7c6c900b5fc9
Oleg <olezhkabobrov@gmail.com>
2021-06-03 16:50:35 +0300

    Fixed hide label

--- Server/include/InitialData.h
+++ Server/include/InitialData.h
@@ -2,10 +2,10 @@
 
 struct InitialData {
     static const int players_amount = 1;
-    static const int time_for_one_task = 10; // in seconds
+    static const int time_for_one_task = 12; // in seconds
     static const int slider_positions = 4;
-    static const int tasks_to_be_done_in_round = 200;
-    static const int amount_of_fails_allowed = 20;
+    static const int tasks_to_be_done_in_round = 20;
+    static const int amount_of_fails_allowed = 30;
     static const int blocks_per_user = 6; // tools on the screen
     static int tool_count;
     static const int dial_positions = 10;
